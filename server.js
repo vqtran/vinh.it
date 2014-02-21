e37800eaa8db14c46d0d52d7465f391769a4c826
Vinh Tran <vinh_tran@brown.edu>
2014-02-21 13:51:35 -0500

    Upgrade to less-middleware

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
 var express = require('express');
 var engine = require('ejs-locals');
+var lessMiddleware = require('less-middleware');
 var fs = require('fs');
-var sass = require('./app/sass-build.js');
 var error = require('./app/util/error.js');
 
 var app = express();
 
-sass.build(__dirname + '/sass/', __dirname + '/public/stylesheets/');
-app.use('/', express.static(__dirname + '/public'));
-app.engine('ejs', engine);
-app.set('view engine', 'ejs');
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(error.logErrors);
-app.use(error.clientErrorHandler);
-app.use(error.errorHandler);
+app.configure(function () {
+    var less = lessMiddleware({
+                dest: __dirname + '/public',
+                src: __dirname + '/assets',
+                yuicompress: true
+            });
+    app.use(less);
+    app.engine('ejs', engine);
+    app.set('view engine', 'ejs');
+    app.use('/', express.static(__dirname + '/public'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(error.logErrors);
+    app.use(error.clientErrorHandler);
+    app.use(error.errorHandler);
+});
 
 app.get('/', function(req, res) {
     res.render('index');
